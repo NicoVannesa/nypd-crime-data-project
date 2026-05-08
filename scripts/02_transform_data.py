b08e9629ef9da7ad786534204e807fd5a885b5dc
Nico Vannesa <andromachintoupi@gmail.com>
2026-05-08 19:36:18 -0400

    Add data transformation script for raw data loading

---
+++ scripts/02_transform_data.py
@@ -0,0 +1,11 @@
+#Data transformation script
+
+import pandas as pd
+
+#Load raw data
+
+df = pd.read_csv("data/raw/nypd_complaint_raw.csv")
+
+print("Raw data loaded successfully.")
+
+# More transformations will go here
