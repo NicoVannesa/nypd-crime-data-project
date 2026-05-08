712e743188604d811590e9ff6320ead78c5a7fbf
Nico Vannesa <andromachintoupi@gmail.com>
2026-05-08 19:37:26 -0400

    Create Fact_Crime_Complaints table in warehouse

---
+++ scripts/03_load_to_warehouse.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Fact_Crime_Complaints (
+    Fact_ID INT PRIMARY KEY,
+    Date_ID INT,
+    Location_ID INT,
+    Crime_ID INT,
+    Premises_ID INT,
+    Suspect_ID INT,
+    Complaint_Number VARCHAR(50)
+);
